032efc8fe41cd39a38e54e8f041b979a084827b0
khy00000 <lightram23@naver.com>
2025-05-20 13:28:58 +0900

    GLTFLoader 완료

--- script.js
+++ script.js
@@ -42,3 +42,46 @@ scene.add(filllight);
 
 const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 2);
 hemiLight.position.set(0, 25, 0);
+scene.add(hemiLight);
+
+//모델에 따라 밝기 조정
+
+// 기본 애니메이션
+function basicAnimate() {
+    renderer.render(scene, camera);
+    requestAnimationFrame(basicAnimate);
+}
+basicAnimate();
+
+// 모델 로더 셋팅
+let model;
+const loader = new THREE.GLTFLoader();
+loader.load("./assets/josta.glb", function (gltf) {
+    model = gltf.scene;
+    model.traverse((node) => {
+        if(node.isMesh){
+            if (node.material){
+                node.material.metalness = 0.3;
+                node.material.roughness = 0.4;
+                node.material.envMapIntensity = 1.5;
+            }
+            node.castShadow = true;
+            node.receiveShadow = true;
+        }
+    });
+
+    const box = new THREE.Box3().setFromObject(model);
+    const center = box.getCenter(new THREE.Vector3());
+    model.position.sub(center);
+    scene.add(model);
+
+    const size = box.getSize(new THREE.Vector3());
+    const maxDim = Math.max(size.x, size.y, size.z);
+    camera.position.z = maxDim * 1.5;
+
+    // model.scale.set(0, 0, 0);
+    playInitialAnimaiton();
+
+    cancelAnimationFrame(basicAnimate);
+    animate();
+});
